b7d6b73aa1da4fe46bd156c0607b47f274c435f2
Paul Graham <paul@smartcasual.io>
2021-10-27 11:55:36 +0100

    Add TextInput component
    Added the `TextInput` component, along with unit tests and Storybook stories.

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,9 +1,11 @@
 import Details from './Details';
 import Panel from './Panel';
+import TextInput from './TextInput';
 import Utils from './utils/Utils';
 
 export {
   Details,
   Panel,
+  TextInput,
   Utils
 };
