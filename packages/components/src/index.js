378c0b51256c20bdf6dd66b836aaf141a46b8dbe
Paul Graham <paul@smartcasual.io>
2021-10-27 12:02:10 +0100

    Add Label component
    Added the `Tag` component, along with unit tests and Storybook stories.

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,10 +1,12 @@
 import Details from './Details';
+import Label from './Label';
 import Panel from './Panel';
 import Tag from './Tag';
 import Utils from './utils/Utils';
 
 export {
   Details,
+  Label,
   Panel,
   Tag,
   Utils
