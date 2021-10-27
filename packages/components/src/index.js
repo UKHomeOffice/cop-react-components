81401b54caf637f54ee0cccfc4c604b89002b607
Paul Graham <paul@smartcasual.io>
2021-10-27 12:15:36 +0100

    Add VisuallyHidden component
    Added the `VisuallyHidden` component, along with unit tests and a Storybook story.

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -3,11 +3,13 @@ import Label from './Label';
 import Panel from './Panel';
 import Tag from './Tag';
 import Utils from './utils/Utils';
+import VisuallyHidden from './VisuallyHidden';
 
 export {
   Details,
   Label,
   Panel,
   Tag,
-  Utils
+  Utils,
+  VisuallyHidden
 };
