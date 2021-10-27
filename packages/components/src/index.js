60d62f1226c3382490fddad9d3a0f283694cfec7
Paul Graham <paul@smartcasual.io>
2021-10-27 12:10:48 +0100

    Add Hint component
    Added the `Hint` component, along with unit tests and Storybook stories.

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,10 +1,12 @@
 import Details from './Details';
+import Hint from './Hint';
 import Panel from './Panel';
 import Tag from './Tag';
 import Utils from './utils/Utils';
 
 export {
   Details,
+  Hint,
   Panel,
   Tag,
   Utils
