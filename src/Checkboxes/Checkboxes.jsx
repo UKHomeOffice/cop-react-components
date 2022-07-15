c60325ea501081a66ce6664536c87aed8004082a
andrewgibson-ibm <andrew.gibson@digital.homeoffice.gov.uk>
2022-07-15 13:25:19 +0100

    Added functionality for exclusive checkboxes

--- src/Checkboxes/Checkboxes.jsx
+++ src/Checkboxes/Checkboxes.jsx
@@ -24,13 +24,41 @@ const Checkboxes = ({
 }) => {
   const classes = classBuilder(classBlock, classModifiers, className);
 
+  // Selection is an array of the options which have been selected
+  // Exclusive Flag is true when an option with the exclusive behaviour is checked
+  // Exclusive Option is the value of the checked exclusive option
   const [selection, setSelection] = useState(value ? value : []);
+  const [exclusiveFlag, setExclusiveFlag] = useState(false);
+  const [exclusiveOption, setExclusiveOption] = useState('');
+  const [nonExclusiveOption, setNonExclusiveOption] = useState('');
 
-  const updateSelection = ({ target }) => {
-    if (target.checked) {
-      setSelection((prev) => [...prev, target.value]);
+  const updateSelection = (event, option) => {
+    // Destructs the event target (checkbox) to its value and checked boolean
+    // Passing option here allows us to expose the 'behaviour' field
+    const { value: targetValue, checked: isTargetChecked } = event.target;
+
+    // console.log(`targetValue = ${targetValue}, isTargetChecked = ${isTargetChecked}, option = ${JSON.stringify(option, null, 2)}`)
+
+    // If this checkbox is checked then update the selection array with its value
+    if (isTargetChecked) {
+      setSelection((prev) => [...prev, targetValue]);
     } else {
-      setSelection((prev) => prev.filter((s) => s !== target.value));
+      setSelection((prev) => prev.filter((s) => s !== targetValue));
+    }
+
+    // If this option's behaviour is set to 'exclusive' and is checked then set the
+    // exclusiveFlag to true, update the exclusive option to its value and reset the
+    // selection array to only its value
+    if (option.behaviour === 'exclusive') {
+      setExclusiveFlag(isTargetChecked);
+      if (isTargetChecked) {
+        setExclusiveOption(option.value);
+        setSelection([option.value]);
+      }
+    } else if (exclusiveFlag && isTargetChecked) {
+      setExclusiveFlag(false);
+      setNonExclusiveOption([option.value]);
+      setSelection([option.value]);
     }
   };
 
@@ -38,7 +66,30 @@ const Checkboxes = ({
     if (typeof onChange === 'function' && selection !== value) {
       onChange({ target: { name: fieldId, value: selection } });
     }
-  }, [selection, onChange, fieldId, value]);
+
+    // Get a list of the checkbox components
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+    // If the checkbox isn't the exclusive option and the flag is on then uncheck it
+    for (let checkbox of checkboxes) {
+      // console.log(`checkbox = ${checkbox.value}, exclusive option = ${exclusiveOption}`)
+      if (
+        (checkbox.value !== exclusiveOption && exclusiveFlag) ||
+        (checkbox.value === exclusiveOption && !exclusiveFlag)
+      ) {
+        // Either: this isn't the exclusive checkbox and the flag is on so uncheck it
+        // or: this is the exclusive checkbox but the flag is not on so uncheck it
+        checkbox.checked = false;
+      } else if (
+        checkbox.value === nonExclusiveOption &&
+        exclusiveOption !== '' &&
+        !exclusiveFlag) {
+        // This isn't the non-exclusive option and the flag is not on so check it
+        checkbox.checked = true;
+      }
+    }
+
+  }, [selection, onChange, fieldId, value, exclusiveFlag, exclusiveOption, nonExclusiveOption]);
 
   if (readonly) {
     return (
@@ -59,20 +110,28 @@ const Checkboxes = ({
       {options &&
         options.map((option, index) => {
           const optionId = `${id}-${index}`;
+
+          // Checks if this option is in the selection array
           const selected = Array.isArray(value) ? value.includes(option.value) : false;
-          return (
-            <Checkbox
-              key={optionId}
-              id={optionId}
-              name={`${name}-${index}`}
-              option={option}
-              selected={selected}
-              onChange={updateSelection}
-              classBlock={classBlock}
-              classModifiers={classModifiers}
-              className={className}
-            />
-          );
+
+          // Adds a divider between options
+          if (typeof option === 'string') {
+            return <div className={classes('divider')} key={optionId}>{option}</div>;
+          } else {
+            return (
+              <Checkbox
+                key={optionId}
+                id={optionId}
+                name={`${name}-${index}`}
+                option={option}
+                selected={selected}
+                onChange={event => updateSelection(event, option)}
+                classBlock={classBlock}
+                classModifiers={classModifiers}
+                className={className}
+              />
+            );
+          }
         })}
     </div>
   );
@@ -88,6 +147,7 @@ Checkboxes.propTypes = {
         label: PropTypes.string.isRequired,
         hint: PropTypes.string,
         disabled: PropTypes.bool,
+        behaviour: PropTypes.string,
       }),
       PropTypes.string,
     ])
--- src/Checkboxes/Checkboxes.test.js
+++ src/Checkboxes/Checkboxes.test.js
@@ -111,4 +111,212 @@ describe('Checkboxes', () => {
     expect(checkboxItems[2].childNodes[0].checked).toEqual(true); //Third option Wales
 
   });
+
+  it('should create a divider between checkboxes', async () => {
+    const ID = 'checkboxes';
+    const FIELD_ID = 'checkboxesFieldId';
+
+    // Create OPTIONS with divider
+    const OPTIONS_WITH_DIVIDER = [
+      { value: 'england', label: 'England' },
+      { value: 'scotland', label: 'Scotland' },
+      'or',
+      { value: 'wales', label: 'Wales' },
+      { value: 'northern-ireland', label: 'Northern Ireland' },
+    ];
+
+    // Build checkboxes
+    const { container } = render(
+      <Checkboxes
+        data-testid={ID}
+        id={ID}
+        fieldId={FIELD_ID}
+        options={OPTIONS_WITH_DIVIDER}
+      />
+    );
+
+    const wrapper = checkSetup(container, ID);
+
+    // Expected number of HTML elements to be same as provided options
+    expect(wrapper.childNodes.length).toEqual(OPTIONS_WITH_DIVIDER.length);
+
+    // Expect divider fields to be correct
+    const divider = wrapper.childNodes[2];
+    expect(divider.classList).toContain(`${DEFAULT_CLASS}__divider`);
+    expect(divider.innerHTML).toEqual(`or`);
+  });
+
+  it('should deselect all other options when an exclusive option is checked', async () => {
+    const ID = 'checkboxes';
+    const FIELD_ID = 'checkboxesFieldId';
+
+    // Create OPTIONS with an exclusive option
+    const OPTIONS_WITH_EXCLUSIVE = [
+      { value: 'england', label: 'England' },
+      { value: 'scotland', label: 'Scotland' },
+      { value: 'wales', label: 'Wales' },
+      { value: 'northern-ireland', label: 'Northern Ireland' },
+      { 
+        value: 'citizen-of-another-country',
+        label: 'Citizen Of Another Country',
+        behaviour: 'exclusive'
+      },
+    ];
+    const EXCLUSIVE_OPTIONS = [
+      {
+        value: 'citizen-of-another-country',
+        label: 'Citizen Of Another Country',
+        behaviour: 'exclusive'
+      }
+    ];
+    const NON_EXCLUSIVE_OPTIONS = [
+      { value: 'england', label: 'England' },
+      { value: 'scotland', label: 'Scotland' },
+      { value: 'wales', label: 'Wales' },
+      { value: 'northern-ireland', label: 'Northern Ireland' }
+    ];
+
+    // Build checkboxes
+    const { container } = render(
+      <Checkboxes
+        data-testid={ID}
+        id={ID}
+        fieldId={FIELD_ID}
+        options={OPTIONS_WITH_EXCLUSIVE}
+      />
+    );
+
+    const wrapper = checkSetup(container, ID);
+
+    // Set all non-exclusive options to checked
+    NON_EXCLUSIVE_OPTIONS.forEach((opt) => {
+      const indexOfOption = OPTIONS_WITH_EXCLUSIVE.findIndex(OPTION => OPTION.value === opt.value);
+      const item = wrapper.childNodes[indexOfOption];
+      const input = item.childNodes[0];
+      fireEvent.click(document.getElementById(input.id));
+    });
+
+    // Expect all non-exclusive options to be checked
+    NON_EXCLUSIVE_OPTIONS.forEach((opt) => {
+      const indexOfOption = OPTIONS_WITH_EXCLUSIVE.findIndex(OPTION => OPTION.value === opt.value);
+      const item = wrapper.childNodes[indexOfOption];
+      const input = item.childNodes[0];
+      expect(input.checked).toEqual(true);
+    });
+
+    // Expect the exclusive option to be not checked
+    EXCLUSIVE_OPTIONS.forEach((opt) => {
+      const indexOfOption = OPTIONS_WITH_EXCLUSIVE.findIndex(OPTION => OPTION.value === opt.value);
+      const item = wrapper.childNodes[indexOfOption];
+      const input = item.childNodes[0];
+      expect(input.checked).toEqual(false);
+    })
+
+    // Set the exclusive option to checked
+    EXCLUSIVE_OPTIONS.forEach((opt) => {
+      const indexOfOption = OPTIONS_WITH_EXCLUSIVE.findIndex(OPTION => OPTION.value === opt.value);
+      const item = wrapper.childNodes[indexOfOption];
+      const input = item.childNodes[0];
+      fireEvent.click(document.getElementById(input.id));
+    });
+
+    // Expect the exclusive option to be checked
+    EXCLUSIVE_OPTIONS.forEach((opt) => {
+      const indexOfOption = OPTIONS_WITH_EXCLUSIVE.findIndex(OPTION => OPTION.value === opt.value);
+      const item = wrapper.childNodes[indexOfOption];
+      const input = item.childNodes[0];
+      expect(input.checked).toEqual(true);
+    });
+
+    // Expect all non-exclusive options to be now be not checked
+    NON_EXCLUSIVE_OPTIONS.forEach((opt) => {
+      const indexOfOption = OPTIONS_WITH_EXCLUSIVE.findIndex(OPTION => OPTION.value === opt.value);
+      const item = wrapper.childNodes[indexOfOption];
+      const input = item.childNodes[0];
+      expect(input.checked).toEqual(false);
+    });
+  });
+
+  it('should deselect all exclusive options when a non-exclusive option is checked', async () => {
+    const ID = 'checkboxes';
+    const FIELD_ID = 'checkboxesFieldId';
+
+    // Create OPTIONS with an exclusive option
+    const OPTIONS_WITH_EXCLUSIVE = [
+      { value: 'england', label: 'England' },
+      { value: 'scotland', label: 'Scotland' },
+      { value: 'wales', label: 'Wales' },
+      { value: 'northern-ireland', label: 'Northern Ireland' },
+      { 
+        value: 'citizen-of-another-country',
+        label: 'Citizen Of Another Country',
+        behaviour: 'exclusive'
+      },
+    ];
+    const EXCLUSIVE_OPTIONS = [
+      {
+        value: 'citizen-of-another-country',
+        label: 'Citizen Of Another Country',
+        behaviour: 'exclusive'
+      }
+    ];
+    const NON_EXCLUSIVE_OPTIONS = [
+      { value: 'england', label: 'England' },
+      { value: 'scotland', label: 'Scotland' },
+      { value: 'wales', label: 'Wales' },
+      { value: 'northern-ireland', label: 'Northern Ireland' }
+    ];
+
+    // Build checkboxes
+    const { container } = render(
+      <Checkboxes
+        data-testid={ID}
+        id={ID}
+        fieldId={FIELD_ID}
+        options={OPTIONS_WITH_EXCLUSIVE}
+      />
+    );
+
+    const wrapper = checkSetup(container, ID);
+
+    // Set the exclusive option to checked
+    EXCLUSIVE_OPTIONS.forEach((opt) => {
+      const indexOfOption = OPTIONS_WITH_EXCLUSIVE.findIndex(OPTION => OPTION.value === opt.value);
+      const item = wrapper.childNodes[indexOfOption];
+      const input = item.childNodes[0];
+      fireEvent.click(document.getElementById(input.id));
+    });
+
+    // Expect the exclusive option to be checked
+    EXCLUSIVE_OPTIONS.forEach((opt) => {
+      const indexOfOption = OPTIONS_WITH_EXCLUSIVE.findIndex(OPTION => OPTION.value === opt.value);
+      const item = wrapper.childNodes[indexOfOption];
+      const input = item.childNodes[0];
+      expect(input.checked).toEqual(true);
+    });
+
+    // Expect the non-exclusive options to be not checked
+    NON_EXCLUSIVE_OPTIONS.forEach((opt) => {
+      const indexOfOption = OPTIONS_WITH_EXCLUSIVE.findIndex(OPTION => OPTION.value === opt.value);
+      const item = wrapper.childNodes[indexOfOption];
+      const input = item.childNodes[0];
+      expect(input.checked).toEqual(false);
+    })
+
+    // Set all non-exclusive options to checked
+    NON_EXCLUSIVE_OPTIONS.forEach((opt) => {
+      const indexOfOption = OPTIONS_WITH_EXCLUSIVE.findIndex(OPTION => OPTION.value === opt.value);
+      const item = wrapper.childNodes[indexOfOption];
+      const input = item.childNodes[0];
+      fireEvent.click(document.getElementById(input.id));
+    });
+
+    // Expect the exclusive option to be now not checked
+    EXCLUSIVE_OPTIONS.forEach((opt) => {
+      const indexOfOption = OPTIONS_WITH_EXCLUSIVE.findIndex(OPTION => OPTION.value === opt.value);
+      const item = wrapper.childNodes[indexOfOption];
+      const input = item.childNodes[0];
+      expect(input.checked).toEqual(false);
+    });
+  });
 });
